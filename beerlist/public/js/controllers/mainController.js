2fdc2710168f298b32493018c6f1768512707e29
talpod <talpod@gmail.com>
2017-03-09 15:46:14 +0200

    server side added

--- beerlist/public/js/controllers/mainController.js
+++ beerlist/public/js/controllers/mainController.js
@@ -5,15 +5,21 @@
     console.log("button works");
     var x = document.forms["myForm"]["bname"].value;
     var y = document.forms["myForm"]["bimage"].value;
+    var beerRating = document.forms["myForm"]["brating"].value;
     if (x == "" || y == "") {
         alert("Please fill in the form. All the details on this form are mandatory!");
         return false;
     }
+else if (beerRating > 5) {
+  alert("Please choose a rating for this beer between 1 and 5.")
+  return false;
+}
     else {
     var beer = {
       name: $scope.name,
       style: $scope.style,
       abv: $scope.abv,
+      rating: $scope.rating,
       image: $scope.image
     }
     };
--- beerlist/server.js
+++ beerlist/server.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/beers');
+var Beer = require("./BeerModel");
+
+// var Beer = require('./models/')
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
-app.listen(8000, function() {
-  console.log("Fullstack project. Listening on 8000.")
+app.get('/', function(req, res) {
+    res.send('Testing Server')
+})
 
+app.get('/beers', function(req, res, next) {
+    Beer.find(function(error, beers) {
+        if (error) {
+            console.error(error)
+            return next(error);
+        } else {
+            res.send(beers);
+        }
+    });
 });
 
-app.get('/beers', function (req, res, next) {
-  res.json({beers: [
-    { name: '512 IPA', style: 'IPA', image_url: 'http://bit.ly/1XtmB4d', abv: 5 },
-    { name: '512 Pecan Porter', style: 'Porter', image_url: 'http://bit.ly/1Vk5xj4', abv: 4 }
-  ]});
+app.listen('8000', function() {
+    console.log("yo yo yo, on 8000 bro");
+});
+app.post('/beers', function(req, res, next) {
+    res.send('POST!');
 });
